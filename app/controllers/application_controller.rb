6bd0f7a1b3ddde6e758f4fa3531bc69011ebacc9
Jeff Pole <jeff.c.pole@gmail.com>
2013-01-15 12:37:32 +0000

    Home page is dashboard for users who auto sign-in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,11 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || session[:user_return_to] || root_path
+    if session[:user_return_to] = root_path
+      dashboard_path
+    else
+      session[:user_return_to] || root_path
+    end
   end
 
   protected
