4608e7bf51f050c9766c3a3bcdf9adfc8d103cbd
Jeff Pole <jeff.c.pole@gmail.com>
2013-01-21 14:26:05 +0000

    Edited code for redirect after career advice request

--- app/controllers/career_advice_requests_controller.rb
+++ app/controllers/career_advice_requests_controller.rb
@@ -7,7 +7,11 @@ class CareerAdviceRequestsController < ApplicationController
     @career_advice_request = CareerAdviceRequest.new(params[:career_advice_request])
     if @career_advice_request.save
       flash[:"alert-success"] = "Thanks! We've received your information and we'll be in touch soon!"
-      redirect_to('/')
+      if user_signed_in?
+        redirect_to dashboard_path
+      else
+        redirect_to root_path
+      end
     else
       render 'new'
     end
