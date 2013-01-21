6e432486d55ba8059c825030aacbeeac660b9166
Jeff Pole <jeff.c.pole@gmail.com>
2013-01-21 14:26:49 +0000

    Edited code for redirect after chat request

--- app/controllers/chat_requests_controller.rb
+++ app/controllers/chat_requests_controller.rb
@@ -7,7 +7,11 @@ class ChatRequestsController < ApplicationController
     @chat_request = ChatRequest.new(params[:chat_request])
     if @chat_request.save
       flash[:"alert-success"] = "Thanks! We've received your contact details and we'll be in touch soon!"
-      redirect_to('/')
+      if user_signed_in?
+        redirect_to dashboard_path
+      else
+        redirect_to root_path
+      end
     else
       render 'new'
     end
