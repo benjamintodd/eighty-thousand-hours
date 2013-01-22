2c46b57efd82f3318508e048051c18ac9e6fb420
Jeff Pole <jeff.c.pole@gmail.com>
2013-01-22 15:42:29 +0000

    Users can now vote for posts without being signed-in

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -16,40 +16,35 @@ class VotesController < ApplicationController
       user_votes = Vote.by_discussion_post(post).by_user(user)
     end
 
-    if user
-      up   = (params[:up] == 'true')
+    up   = (params[:up] == 'true')
 
-      # check if user has already voted for this post
-      if user_votes.empty?
-        if type == :blog
-          post.votes << Vote.new( :user => user, :blog_post => post, :positive => up )
-        else
-          post.votes << Vote.new( :user => user, :discussion_post => post, :positive => up )
-        end
+    # check if user has already voted for this post
+    if user_votes.empty?
+      if type == :blog
+        post.votes << Vote.new( :user => user, :blog_post => post, :positive => up )
       else
-        vote = user_votes.first
-        if (up && vote.positive) || (!up && !vote.positive)
-          # user already upvoted, and clicked up again
-          # so we destroy the vote
-          # and vice versa
-          vote.destroy
-        else
-          # we had an upvote, and user clicked Down
-          # so we change the upvote to a downvote
-          # or vice versa
-          vote.positive = !vote.positive
-          vote.save
-        end
+        post.votes << Vote.new( :user => user, :discussion_post => post, :positive => up )
       end
-
-      @arrow_element = "#{up ? 'up-' : 'down-'}vote-#{post.id}"
-      @arrow_element_other = "#{up ? 'down-' : 'up-'}vote-#{post.id}"
-      @vote_element  = "total-votes-#{post.id}"
-      @net_votes = post.net_votes
     else
-      @error_element = "vote-error-#{post.id}"
-      @error = "NO!"
+      vote = user_votes.first
+      if (up && vote.positive) || (!up && !vote.positive)
+        # user already upvoted, and clicked up again
+        # so we destroy the vote
+        # and vice versa
+        vote.destroy
+      else
+        # we had an upvote, and user clicked Down
+        # so we change the upvote to a downvote
+        # or vice versa
+        vote.positive = !vote.positive
+        vote.save
+      end
     end
+
+    @arrow_element = "#{up ? 'up-' : 'down-'}vote-#{post.id}"
+    @arrow_element_other = "#{up ? 'down-' : 'up-'}vote-#{post.id}"
+    @vote_element  = "total-votes-#{post.id}"
+    @net_votes = post.net_votes
 
     respond_to do |format|
       format.js { render :layout => false }
