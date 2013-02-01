764dde0e265d332088b106e06e860c7d52966888
Jeff Pole <jeff.c.pole@gmail.com>
2013-02-01 13:23:11 +0000

    Styling of comments on blog posts

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -48,17 +48,32 @@ class Comment < ActiveRecord::Base
     end
   end
 
+
+  #methods related to nested comments
+
   def get_top_parent_comment
-    #debugger
     if self.commentable_type == "BlogPost" || self.commentable_type == "DiscussionPost"
       self
     else  #must be a nested comment
       parent = Comment.find_by_id(self.commentable_id)
-      #debugger
       while parent.commentable_type != "BlogPost" && parent.commentable_type != "DiscussionPost"
         parent = Comment.find_by_id(parent.commentable_id)
       end
       parent
+    end
+  end
+
+  def get_depth
+    if self.commentable_type == "BlogPost" || self.commentable_type == "DiscussionPost"
+      return 0
+    else
+      count = 1
+      parent = Comment.find_by_id(self.commentable_id)
+      while parent.commentable_type != "BlogPost" && parent.commentable_type != "DiscussionPost"
+        parent = Comment.find_by_id(parent.commentable_id)
+        count+=1
+      end
+      return count
     end
   end
 
@@ -81,7 +96,6 @@ class Comment < ActiveRecord::Base
         result = false
       end
     end
-
 
     # also check that it has an id of something of type commentable
     if self.commentable_id.nil?
