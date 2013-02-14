b43453f3e192683bc74711b6916e1686bfa4d550
Jeff Pole <jeff.c.pole@gmail.com>
2013-02-14 09:39:24 +0000

    Created mailer action for contacting users

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,4 +5,19 @@ class UserMailer < ActionMailer::Base
     @name = user.first_name
     mail to: user.email, subject: "[80,000 Hours] New account"
   end
+
+  def contact_user(sender, recipient, subject, body)
+  	# get the name of user sending the message
+  	@contact_name = sender.name
+
+  	# get the email of the user sending the message
+  	@contact_email = sender.email
+
+  	# get the subject and body of the message
+  	@subject = subject
+  	@body = body
+
+  	mail(to: recipient.email, \
+  		subject: "[80,000 Hours] You have been contacted by another member")
+  end
 end
