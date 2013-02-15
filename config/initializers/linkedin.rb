e5ecab62864b5e1e5db604adf18eef59bc44ad66
Jeff Pole <jeff.c.pole@gmail.com>
2013-02-15 15:55:21 +0000

    Added config constants for linkedin requests

--- config/initializers/linkedin.rb
+++ config/initializers/linkedin.rb
@@ -1,3 +1,8 @@
+LINKEDIN_CONFIG_BASIC = { :site => 'https://api.linkedin.com', :authorize_path => '/uas/oauth/authenticate', :request_token_path =>'/uas/oauth/requestToken?scope=r_basicprofile', :access_token_path => '/uas/oauth/accessToken' }
+LINKEDIN_CONFIG_BASIC_EMAIL = { :site => 'https://api.linkedin.com', :authorize_path => '/uas/oauth/authenticate', :request_token_path =>'/uas/oauth/requestToken?scope=r_basicprofile+r_emailaddress', :access_token_path => '/uas/oauth/accessToken' }
+LINKEDIN_CONFIG_FULL = { :site => 'https://api.linkedin.com', :authorize_path => '/uas/oauth/authenticate', :request_token_path =>'/uas/oauth/requestToken?scope=r_fullprofile', :access_token_path => '/uas/oauth/accessToken' }
+LINKEDIN_CONFIG_INVITE = { :site => 'https://api.linkedin.com', :authorize_path => '/uas/oauth/authenticate', :request_token_path =>'/uas/oauth/requestToken?scope=w_messages', :access_token_path => '/uas/oauth/accessToken' }
+
 LinkedIn::Client.class_eval do
   def get_email
     path = "/people/~/email-address"
