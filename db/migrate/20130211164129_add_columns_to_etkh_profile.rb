9496dc0a194f2dc5d42bab3c85299c565094aa94
Jeff Pole <jeff.c.pole@gmail.com>
2013-02-20 10:11:49 +0000

    Removed add profile completeness column to profile table

--- db/migrate/20130211164129_add_columns_to_etkh_profile.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnsToEtkhProfile < ActiveRecord::Migration
-  def up
-  	add_column :etkh_profiles, :profile_completeness, :integer
-  end
-
-  def down
-  	remove_column :etkh_profiles, :profile_completeness
-  end
-end
