bc6af225849aaea8e2762d47d3cc714ea02fcd5b
Jeff Pole <jeff.c.pole@gmail.com>
2013-02-26 11:28:08 +0000

    original version of positions WIP

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -12,11 +12,15 @@ class PositionsController < ApplicationController
   end
 
   def edit
-
+    @position = Position.find_by_id(params[:id])
+    render 'etkh_profiles/positions/new'
   end
 
   def update
-
+    debugger
+    @position = Position.find_by_id(params[:id])
+    @position.update_attributes(params[:position])
+    render 'etkh_profiles/positions/update'
   end
 
   def destroy
