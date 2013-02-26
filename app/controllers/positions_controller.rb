12eb9abd7b6149d37a8da511839b9d07bd260dbf
Jeff Pole <jeff.c.pole@gmail.com>
2013-02-26 13:15:44 +0000

    WIP Revamped rendering of positions

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -1,26 +1,29 @@
 class PositionsController < ApplicationController
   def new
     @position = Position.new
-    render 'etkh_profiles/positions/new'
+    render 'etkh_profiles/positions/show_form'
   end
 
   def create
     @new_position = Position.new(params[:position])
     @new_position.etkh_profile_id = params[:position][:etkh_profile_id]
     @new_position.save
-    render 'etkh_profiles/positions/create'
+
+    @ordered_positions = current_user.etkh_profile.positions.order("end_date_year DESC")
+    render 'etkh_profiles/positions/redraw_positions'
   end
 
   def edit
     @position = Position.find_by_id(params[:id])
-    render 'etkh_profiles/positions/new'
+    render 'etkh_profiles/positions/show_form'
   end
 
   def update
-    debugger
     @position = Position.find_by_id(params[:id])
     @position.update_attributes(params[:position])
-    render 'etkh_profiles/positions/update'
+    sleep(1)
+    @ordered_positions = current_user.etkh_profile.positions.order("end_date_year DESC")
+    render 'etkh_profiles/positions/redraw_positions'
   end
 
   def destroy
