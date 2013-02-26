fd7a12f9f1b7b7395fa94ea695886549f10a9673
Jeff Pole <jeff.c.pole@gmail.com>
2013-02-26 15:01:06 +0000

    Moved JS to separate file

---
+++ app/assets/javascripts/position-form.js
@@ -0,0 +1,32 @@
+$(document).ready( function() {
+  $("#btn-add-position-submit").click( function() {
+    var edit = $("#data").data('edit');
+    var profile_id = $("#data").data('profile-id');
+    
+    if (edit == false)
+    {
+      var valuesToSubmit = $("#new_position").serialize();
+      $.ajax({
+        type: 'POST',
+        url: '/members/' + profile_id + '/positions',
+        data: valuesToSubmit
+      });
+    }
+    else
+    {
+      var id = $("#id").data('id');
+      var valuesToSubmit = $("#edit_position_" + id).serialize();
+      $.ajax({
+        type: 'PUT',
+        url: '/members/' + profile_id + '/positions/' + id,
+        data: valuesToSubmit
+      });
+    }
+    return false;
+  });
+
+  $("#btn-add-position-cancel").click( function() {
+    $("#position-form").html("");
+    return false;
+  });
+});
