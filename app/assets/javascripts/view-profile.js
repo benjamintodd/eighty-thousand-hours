0d0ff4b471fe2c53233acece57c9b27139ed10b6
Jeff Pole <jeff.c.pole@gmail.com>
2013-03-06 12:13:37 +0000

    sections with full page left-filler

--- app/assets/javascripts/view-profile.js
+++ app/assets/javascripts/view-profile.js
@@ -1,4 +1,11 @@
 $(document).ready( function() {
+  
+  // set background colour to a light grey
+  $("body").css('background-color','rgb(233,233,233)');
+
+  var full_height = $(".container").children("#content").children(".row").children(".span12").css('height');
+  $(".filler").css('height',full_height);
+
   $("#causes-list").children("li:gt(3)").hide();
   $("#see-more-causes").click( function(){
     $("#causes-list").children("li:gt(3)").show();
