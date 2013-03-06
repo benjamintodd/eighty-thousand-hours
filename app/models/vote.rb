9052e25f03bd9cc2fe08bec11aaac95933725443
Jeff Pole <jeff.c.pole@gmail.com>
2013-03-06 17:44:28 +0000

    Added scopes for getting votes by post type to vote model

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -9,4 +9,7 @@ class Vote < ActiveRecord::Base
   scope :downvotes, where(:positive => false)
 
   scope :recent, order("created_at DESC").limit(3)
+  scope :belongs_to_blog_post, where(post_type: "BlogPost")
+  scope :belongs_to_discussion_post, where(post_type: "DiscussionPost")
+  scope :belongs_to_comment, where(post_type: "Comment")
 end
