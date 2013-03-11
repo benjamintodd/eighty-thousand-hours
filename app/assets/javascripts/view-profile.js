07dc0335af6ecb60cac60b514d0b38e18a9914c5
Jeff Pole <jeff.c.pole@gmail.com>
2013-03-11 14:23:46 +0000

    Fixed stretching of images to fit avatar photo boxes

--- app/assets/javascripts/view-profile.js
+++ app/assets/javascripts/view-profile.js
@@ -17,6 +17,25 @@ $(document).ready( function() {
       }
     });
 
+    // set image dimensions dynamically to avoid stretching
+    // short delay to allow images to load
+    setTimeout( function() {
+      $(".avatar").find("img").each( function() {
+        var img = new Image();
+        img.src = $(this).attr('src');
+
+        // set shorter side to maximum so that image fills square without stretching
+        if (img.width > img.height)
+        {
+          $(this).css('height', '100%');
+        }
+        else
+        {
+          $(this).css('width', '100%');
+        }
+      });
+    }, 50);
+
     // Truncate lists
     var fadeInTime = 800;
 
