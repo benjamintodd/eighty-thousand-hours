cff84dc0f40156da1e54859f2985aa3d8d9573fe
Jeff Pole <jeff.c.pole@gmail.com>
2013-03-13 21:53:58 +0000

    New slide down animation of search form and styling to form

--- app/assets/javascripts/members-page.js
+++ app/assets/javascripts/members-page.js
@@ -1,14 +1,46 @@
+var fireCount2 = 0;
 $(document).ready( function() {
-  // $("#members-list").pageless({
-  //   totalPages: 100,
-  //   url: '/members/get_more_members',
-  //   //msgStyles: {'color': '#000000', 'font-size': '1em'},
-  //   //loaderMsg: 'Loading more results',   
-  //   loaderImg: '/images/load.gif',
-  //   distance: '2500'
-  // });
+  fireCount2++;
+  if (fireCount2 > 1)
+  {
+    $("#members-list").pageless({
+      totalPages: 100,
+      url: '/members/get_more_members',
+      //msgStyles: {'color': '#000000', 'font-size': '1em'},
+      //loaderMsg: 'Loading more results',   
+      loaderImg: '/images/load.gif',
+      distance: '2500'
+    });
 
-  $("#advanced-search-link").click( function() {
-  	$("#advanced-search").show("slide", { direction: "up" }, 1000);
-  });
+    // animate search form when advanced search is
+    $("#advanced-search").click( function() {
+    	$(this).fadeOut(300);
+      $(".button").hide();
+
+      // animation occurs within 700 milliseconds
+      var time  = 700;
+
+      // show and animate hidden elements of form
+      $("#hidden-inputs").show();
+      $("#hidden-labels").show();
+      $("#hidden-inputs").offset({top: 0});
+      $("#hidden-labels").offset({top: 0});
+      $("#hidden-inputs").animate({top: 0}, time);
+      $("#hidden-labels").animate({top: 0}, time);
+
+      // change dimensions of search form and shift up slightly
+      $("#search-form").animate({
+        width: 360,
+        height: 300
+      }, time);
+      $(".inputs").animate({top: -5}, time);
+
+      // button behaves strangely during animation so hide it and then fade it back in
+      setTimeout( function() {
+        $(".button").css('margin-left', '130px');
+        $(".button").css('top', '-5px');
+        $(".button").fadeIn(500);
+      }, 500);
+    });
+  }
 });
