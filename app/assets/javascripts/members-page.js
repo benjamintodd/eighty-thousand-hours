7a427f7e7e6e251097085737a2791188936b1755
Jeff Pole <jeff.c.pole@gmail.com>
2013-03-13 15:27:31 +0000

    Moved resize image JS inside profiles_selection partial

--- app/assets/javascripts/members-page.js
+++ app/assets/javascripts/members-page.js
@@ -1,5 +1,4 @@
 $(document).ready( function() {
-
   $("#members-list").pageless({
     totalPages: 100,
     url: '/members/get_more_members',
@@ -8,35 +7,4 @@ $(document).ready( function() {
     loaderImg: '/images/load.gif',
     distance: '2500'
   });
-
-  // set image dimensions to avoid stretching
-  // short delay to allow images to load
-  setTimeout( function() {
-    resizeImages();
-  }, 250);
-
-  // since this page takes a long time to load all the images, redo the resize images function to make sure all images are resized correctly
-  setTimeout( function() {
-    resizeImages();
-  }, 500);
-  setTimeout( function() {
-    resizeImages();
-  }, 1000);
 });
-
-function resizeImages()
-{
-  $(".avatar").find("img").each( function() {
-    var img = new Image();
-    img.src = $(this).attr('src');
-
-    if (img.width > img.height)
-    {
-      $(this).css('height', '100%');
-    }
-    else
-    {
-      $(this).css('width', '100%');
-    }
-  });
-}
