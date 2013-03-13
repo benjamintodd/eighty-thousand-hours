1c3a80be4143dea9d0a367206b6d9729c624d5f3
Jeff Pole <jeff.c.pole@gmail.com>
2013-03-13 17:35:18 +0000

    Started styling search forms; slide-down animation in place

--- app/assets/javascripts/members-page.js
+++ app/assets/javascripts/members-page.js
@@ -1,10 +1,14 @@
 $(document).ready( function() {
-  $("#members-list").pageless({
-    totalPages: 100,
-    url: '/members/get_more_members',
-    //msgStyles: {'color': '#000000', 'font-size': '1em'},
-    //loaderMsg: 'Loading more results',   
-    loaderImg: '/images/load.gif',
-    distance: '2500'
+  // $("#members-list").pageless({
+  //   totalPages: 100,
+  //   url: '/members/get_more_members',
+  //   //msgStyles: {'color': '#000000', 'font-size': '1em'},
+  //   //loaderMsg: 'Loading more results',   
+  //   loaderImg: '/images/load.gif',
+  //   distance: '2500'
+  // });
+
+  $("#advanced-search-link").click( function() {
+  	$("#advanced-search").show("slide", { direction: "up" }, 1000);
   });
 });
