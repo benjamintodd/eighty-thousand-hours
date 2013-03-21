27c11317b66009a4d2b5a6e88a262744a602c13c
Jeff Pole <jeff.c.pole@gmail.com>
2013-03-21 11:02:59 +0000

    Fixed firefox bug in search form

--- app/assets/javascripts/search-form.js
+++ app/assets/javascripts/search-form.js
@@ -8,24 +8,22 @@ $(document).ready( function() {
     var time  = 700;
 
     // show and animate hidden elements of form
-    $("#hidden-inputs").show();
-    $("#hidden-labels").show();
-    $("#hidden-inputs").offset({top: 0});
-    $("#hidden-labels").offset({top: 0});
-    $("#hidden-inputs").animate({top: 0}, time);
-    $("#hidden-labels").animate({top: 0}, time);
+    $(".hidden").show();
+    $(".hidden input").offset({top: 0});
+    $(".hidden label").offset({top: 0});
+    $(".hidden input").animate({top: 0}, time);
+    $(".hidden label").animate({top: 0}, time);
 
     // change dimensions of search form and shift up slightly
     $(".search-form").animate({
       width: 360,
-      height: 300
+      height: 305
     }, time);
-    $(".inputs").animate({top: -5}, time);
 
     // button behaves strangely during animation so hide it and then fade it back in
     setTimeout( function() {
-      $(".button").css('margin-left', '130px');
-      $(".button").css('top', '-5px');
+      $(".button").css('margin-left', '120px');
+      $(".button").css('top', '5px');
       $(".button").fadeIn(500);
     }, 500);
   });
