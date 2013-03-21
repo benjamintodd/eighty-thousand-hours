49062cf6b522992bfbe6527f70d6d090f842291a
Jeff Pole <jeff.c.pole@gmail.com>
2013-03-21 12:03:30 +0000

    Changed user paths to profile

--- app/helpers/blog_posts_helper.rb
+++ app/helpers/blog_posts_helper.rb
@@ -2,8 +2,8 @@ module BlogPostsHelper
   include ActsAsTaggableOn::TagsHelper
 
   def link_to_blog_post_author(post)
-    if post.user
-      (link_to post.user.name, user_path(post.user))
+    if post.user && post.user.etkh_profile
+      (link_to post.user.name, "/members/#{post.user.slug}")
     else
       (link_to post.author, { :controller => 'blog_posts', :action => :author, :id => post.author })
     end
--- app/helpers/discussion_posts_helper.rb
+++ app/helpers/discussion_posts_helper.rb
@@ -1,7 +1,7 @@
 module DiscussionPostsHelper
   def link_to_discussion_post_author( post )
-    if post.user
-      link_to post.user.name, user_path( post.user )
+    if post.user && post.user.etkh_profile
+      link_to post.user.name, "/members/#{post.user.slug}"
     else
       link_to post.name, (discussion_post_path( post ))
     end
