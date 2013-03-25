940c46baf15576e29e846fe3e9b53400f7bd5d01
Jeff Pole <jeff.c.pole@gmail.com>
2013-03-25 00:43:08 +0000

    Fixed problem with add indexes migration

--- db/migrate/20130324214546_add_indexes.rb
+++ db/migrate/20130324214546_add_indexes.rb
@@ -6,16 +6,15 @@ class AddIndexes < ActiveRecord::Migration
   	add_index :blog_posts, :created_at, name: "index_blog_posts_on_created_at"
   	add_index :comments, :user_id, name: "index_comments_on_user_id"
   	add_index :discussion_posts, :user_id, name: "index_discussion_posts_on_user_id"
-  	add_index :donations, :user_id, name: "index_donations_on_user_id"
   	add_index :educations, :etkh_profile_id, name: "index_educations_on_etkh_profile_id"
   	add_index :educations, :member_info_id, name: "index_educations_on_member_info_id"
   	add_index :etkh_profiles, :user_id, name: "index_etkh_profiles_on_user_id", unique: true
-  	add_index :etkh_profiles_profile_option_activities, :etkh_profile_id, name: "index_etkh_profiles_profile_option_activities_on_etkh_profile_id"
-  	add_index :etkh_profiles_profile_option_causes, :etkh_profile_id, name: "index_etkh_profiles_profile_option_causes_on_etkh_profile_id"
+  	add_index :etkh_profiles_profile_option_activities, :etkh_profile_id, name: "index_profile_option_activities_on_etkh_profile_id"
+  	add_index :etkh_profiles_profile_option_causes, :etkh_profile_id, name: "index_profile_option_causes_on_etkh_profile_id"
   	add_index :linkedin_infos, :user_id, name: "index_linkedin_infos_on_user_id", unique: true
-  	add_index :member_infos, :user_id, name: "index_member_infos_on_user_id"
+  	add_index :member_infos, :user_id, name: "index_member_infos_on_user_id", unique: true
   	add_index :positions, :etkh_profile_id, name: "index_positions_on_etkh_profile_id"
-  	add_index :positions, :member_info_id, name: "index_positions_on_member_info_id", unique: true
+  	add_index :positions, :member_info_id, name: "index_positions_on_member_info_id"
   	add_index :votes, :user_id, name: "index_votes_on_user_id"
   end
 end
