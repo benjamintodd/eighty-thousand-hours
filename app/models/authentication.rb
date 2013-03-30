57ffc2ecad6bed74ad2a647cdd39332ece833de6
Jeff Pole <jeff.c.pole@gmail.com>
2013-03-30 19:25:06 +0000

    Fixed bug in date from fb profiles

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -8,59 +8,63 @@ class Authentication < ActiveRecord::Base
     #user.external_facebook = omniauth.extra.raw_info.link if omniauth.extra.raw_info.link
     user.external_facebook = omniauth['info']['urls']['Facebook'] if omniauth['info']['urls'] && omniauth['info']['urls']['Facebook']
 
-    if omniauth.extra && omniauth.extra.raw_info
-      # if other info present add to member_info table
-      if user.member_info
-        member_info = user.member_info
-      else
-        member_info = MemberInfo.new
-        member_info.user_id = user.id
-      end
+    begin
+      if omniauth.extra && omniauth.extra.raw_info
+        # if other info present add to member_info table
+        if user.member_info
+          member_info = user.member_info
+        else
+          member_info = MemberInfo.new
+          member_info.user_id = user.id
+        end
 
-      member_info.gender = omniauth.extra.raw_info.gender if omniauth.extra.raw_info.gender
-      if positions = omniauth.extra.raw_info.work
-        positions.each do |position|
-          new_pos = Position.new
-          new_pos.position = position.position.name if position.position.name
-          new_pos.organisation = position.employer.name if position.employer && position.employer.name
-          
-          if position.start_date
-            date = position.start_date.split("-")
-            if date.length == 1
-              new_pos.start_date_year = DateTime.new(date[0].to_i)
-            elsif date.length == 2
-              new_pos.start_date_year = DateTime.new(date[0].to_i, date[1].to_i)
-            elsif date.length == 3
-              new_pos.start_date_year = DateTime.new(date[0].to_i, date[1].to_i, date[2].to_i)
+        member_info.gender = omniauth.extra.raw_info.gender if omniauth.extra.raw_info.gender
+        if positions = omniauth.extra.raw_info.work
+          positions.each do |position|
+            new_pos = Position.new
+            new_pos.position = position.position.name if position.position.name
+            new_pos.organisation = position.employer.name if position.employer && position.employer.name
+            
+            if position.start_date
+              date = position.start_date.split("-")
+              if date[0].to_s != "0000"
+                new_pos.start_date_year = date[0].to_i
+                new_pos.start_date_year = convert_month(date[1].to_i) if date[1]
+              end
             end
-          end
-          if position.end_date
-            date = position.end_date.split("-")
-            if date.length == 1
-              new_pos.end_date_year = DateTime.new(date[0].to_i)
-            elsif date.length == 2
-              new_pos.end_date_year = DateTime.new(date[0].to_i, date[1].to_i)
-            elsif date.length == 3
-              new_pos.end_date_year = DateTime.new(date[0].to_i, date[1].to_i, date[2].to_i)
+            if position.end_date
+              date = position.end_date.split("-")
+              if date[0].to_s != "0000"
+                new_pos.end_date_year = date[0].to_i
+                new_pos.end_date_month = convert_month(date[1].to_i) if date[1]
+              end
             end
+            
+            new_pos.member_info = member_info
+            new_pos.save
           end
-          
-          new_pos.member_info = member_info
-          new_pos.save
+        end
+        if educations = omniauth.extra.raw_info.education
+          educations.each do |education|
+            new_ed = Education.new
+            new_ed.university = education.school.name if education.school && education.school.name
+            new_ed.end_date_year = education.year.name.to_i if education.year && education.year.name
+            new_ed.member_info = member_info
+            new_ed.save
+          end
         end
       end
-      if educations = omniauth.extra.raw_info.education
-        educations.each do |education|
-          new_ed = Education.new
-          new_ed.university = education.school.name if education.school && education.school.name
-          new_ed.end_date_year = DateTime.new(education.year.name.to_i) if education.year && education.year.name
-          new_ed.member_info = member_info
-          new_ed.save
-        end
-      end
-      member_info.save
+    rescue => e
+      p e.message
+    ensure
+      member_info.save if member_info
+      user.save
     end
+  end
 
-    user.save
+  private
+  def self.convert_month(num)
+    months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+    return months[num.to_i]
   end
 end
