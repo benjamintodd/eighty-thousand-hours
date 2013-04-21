8014a90fec526bec88d2b95ffac55612acec05a6
Jeff Pole <jeff.c.pole@gmail.com>
2013-04-21 19:27:03 -0400

    Implemented process to store popular posts in session variable

--- app/helpers/blog_posts_helper.rb
+++ app/helpers/blog_posts_helper.rb
@@ -10,4 +10,21 @@ module BlogPostsHelper
   def link_to_blog_post_contents(post,length=35)
     link_to(truncate(post.title, length: length), blog_post_path( post ))
   end
+
+  def get_popular_posts
+    posts = []
+  	if session[:popular_posts] && (Time.now - session[:popular_posts_updated_at].to_datetime) < 15.minutes
+      session[:popular_posts].each do |post_id|
+  		  posts << BlogPost.find_by_id(post_id)
+      end
+    else
+      posts = BlogPost.by_popularity(5)
+      session[:popular_posts] = []
+      posts.each do |post|
+        session[:popular_posts] << post.id
+      end
+      session[:popular_posts_updated_at] = Time.now.to_s
+    end
+    return posts
+  end
 end
