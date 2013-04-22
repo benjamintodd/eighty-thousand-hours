07e86ec1563a019498ce92dba236619a07e2e86d
Jeff Pole <jeff.c.pole@gmail.com>
2013-04-21 21:55:11 -0400

    Modified blog post images

--- app/models/attached_image.rb
+++ app/models/attached_image.rb
@@ -1,5 +1,6 @@
+# at present used only for images in blog posts
 class AttachedImage < ActiveRecord::Base
-  # paperclip uploads to on S3
+  # paperclip uploads to S3
   has_attached_file :image, {
                       :styles => { :original => "1024x768", :large => "800x800", :medium => "500x500", :small => "200x200>" },
                       :path => "/posts/images/:id/:style/:filename"
@@ -9,6 +10,15 @@ class AttachedImage < ActiveRecord::Base
                             :unless => Proc.new {|m| m[:asset].nil?}
   validates_attachment_content_type :upload, :content_type=>['image/jpeg', 'image/png', 'image/gif'],
                                     :unless => Proc.new {|m| m[:image].nil?}
+  validate :image_aspect_ratio, unless: "errors.any?"
 
   belongs_to :post
+
+  def image_aspect_ratio
+    dimensions = Paperclip::Geometry.from_file(image.queued_for_write[:original].path)
+    ratio = dimensions.width.to_f / dimensions.height.to_f
+    if ratio < 1.3
+      errors.add(:image,'Image is too tall, please update the aspect ratio and try again')
+    end
+  end
 end
