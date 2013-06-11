84a7c652045b253f3e2e5430ae84f56cedf8c5cf
Isaac <isaac@friendly-rat.(none)>
2013-06-11 15:24:01 +0100

    Started compressing Javascripts in dev environment - this was the only way to get it to work properly, need to look at this properly at some point

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,7 +23,12 @@ EightyThousandHours::Application.configure do
   config.action_dispatch.best_standards_support = :builtin
 
   # Do not compress assets
-  config.assets.compress = false
+  config.assets.compress = true
+  config.assets.css_compressor = :yui
+  config.assets.js_compressor = :uglifier
+
+  config.assets.compile = false
+  # config.assets.compress = false
 
   # Expands the lines which load the assets
   config.assets.debug = true
