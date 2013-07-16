86f520ee6f6315f745501c034d25bd628f2f9f5f
Isaac <isaac@friendly-rat.(none)>
2013-07-16 13:46:55 +0100

    Temporarily turn on full error reporting in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ EightyThousandHours::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
   # configure app to use Amazon S3 as an asset host
