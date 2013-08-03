fdab043bd73864b4b41929ddee15ffe07cb2a307
Isaac <isaac@friendly-rat.(none)>
2013-08-03 15:29:41 +0100

    New coaching request page

--- app/models/coaching_request.rb
+++ app/models/coaching_request.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 class CoachingRequest < ActiveRecord::Base
-  attr_accessible :cant_decide, :conterfactual_career_plans, :counterfactual_donation_amount, :counterfactual_donation_target, :current_career_plans, :current_donation_percent, :current_donation_target, :current_situation, :dont_know_options, :email, :name, :other_factors, :questions, :skype, :wants_better_world
+  attr_accessible :cant_decide, :conterfactual_career_plans, :counterfactual_donation_amount, :counterfactual_donation_target, :current_career_plans, :current_donation_percent, :current_donation_target, :current_situation, :dont_know_options, :email, :name, :other_factors, :questions, :skype, :wants_better_world, :other_causes
 
   SituationOptions = {a: "I’ve got a plan for my career and I’d like help on how to accomplish it.",
     b: "I’m trying to choose between two or more options.",
@@ -19,5 +19,22 @@ class CoachingRequest < ActiveRecord::Base
     b: "slightly prevents me",
     c: "prevents me quite a lot",
     d: "very much prevents me"
+  }
+
+  CauseOptions = {
+    a: "Global poverty", 
+    b: "Improving the far future",
+    c: "Ending factory farming",
+    d: "Global prioritisation",
+    e: "Promoting effective altruism",
+    f: "Improving decision making",
+    g: "Meta-research (improving academic research practices)"
+  }
+
+  SwitchingCauseOptions = {
+    a: "Very likely",
+    b: "Likely",
+    c: "Not likely",
+    d: "Highly unlikely"
   }
 end
