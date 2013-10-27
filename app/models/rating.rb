817490ce85c823daafe911d68e17fc4297502e74
Ozzie Gooen <Ozzie@BowLabs.org>
2013-10-27 19:12:03 +0000

    ratings should mostly work now

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,7 +2,7 @@ class Rating < ActiveRecord::Base
   CATEGORIES = %w(overall original practical persuasive transparent accessible engaging)
   belongs_to :blog_post
   belongs_to :user
-  attr_accessible :overall, :original, :practical, :persuasive,:transparent, :accessible, :engaging, :user, :user_id, :blog_post, :blog_post_id
+  attr_accessible :overall, :original, :practical, :persuasive,:transparent, :accessible, :engaging, :comment, :user, :user_id, :blog_post, :blog_post_id
   validates :user, presence: true
   validates :blog_post, presence: true
   validates :overall, :original, :practical, :persuasive, :transparent, :accessible, :engaging, inclusion: { in: (0..10)}
