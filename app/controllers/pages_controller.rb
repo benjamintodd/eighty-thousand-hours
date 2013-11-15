fb8c6e543018f43115040bec6ad3b0ef5c6f36cf
Ozzie Gooen <Ozzie@BowLabs.org>
2013-11-15 13:45:45 +0000

    removed binding.pry because pages endorsements was breaking on production

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -98,7 +98,6 @@ class PagesController < ApplicationController
 
   def endorsements
     @title = 'Endorsements for 80,000 Hours'
-    binding.pry
     @endorsements = t 'endorsements'
     @menu_current = 'Endorsements'
     @menu_root = 'About Us'
